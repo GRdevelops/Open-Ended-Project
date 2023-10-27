8eee536d5bf931b7325bbda45436eb4ce6569aef
GRdevelops <gianniruggieri99@gmail.com>
2023-10-27 08:44:03 +0200

    Updated .gitignore, cleaned css and js

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,15 +3,24 @@
 const modal = document.getElementById('myModal');
 const closeButton = document.getElementsByClassName('close')[0];
 
-const timeToAppear = 3000 // Milliseconds after which the pop-up will appear
+const timeToAppear = 4000; // Milliseconds after which the pop-up will appear
 const timeInterval = 60 * 1000; // <-- Modify this. Prevent the pop-up from reappearing when reloading: Insert interval in milliseconds.
 let minutes = timeInterval / 1000 / 60; // Convert milliseconds to minutes
 
+const modalClosedTime = localStorage.getItem('modalClosed');
+let currentTime = new Date().getTime();
+
 closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-    currentTime = new Date().getTime(); // current timestamp in milliseconds
-    localStorage.setItem('modalClosed', currentTime.toString());
+	modal.style.display = 'none';
+	currentTime = new Date().getTime(); // current timestamp in milliseconds
+	localStorage.setItem('modalClosed', currentTime.toString());
 });
+
+if (!modalClosedTime || currentTime - modalClosedTime >= timeInterval) {
+	setTimeout(() => {
+		modal.style.display = 'block';
+	}, timeToAppear);
+}
 
 // // Close the pop-up by clicking outside the window
 // window.addEventListener('click', (event) => {
@@ -22,58 +31,42 @@ closeButton.addEventListener('click', () => {
 //     }
 // });
 
-const modalClosedTime = localStorage.getItem('modalClosed');
-let currentTime = new Date().getTime();
+// localStorage.removeItem('modalClosed'); // Uncomment to reset pop-up
 
-if (!modalClosedTime || currentTime - modalClosedTime >= timeInterval) {
-    setTimeout(() => {
-        modal.style.display = 'block';
-    }, timeToAppear);
-}
+const timeRemaining = document.getElementById('time-interval');
 
-// localStorage.removeItem('modalClosed'); //Uncomment to reset pop-up
-
-
-const interval = document.getElementById('time-interval');
-
-// Modify html paragraph based on set interval
-if (minutes < 1) { 
-    minutes = minutes * 60;
-    interval.innerHTML = `${minutes} seconds`;
-} 
-else if (minutes === 1) { 
-    interval.innerHTML = `${minutes} minute`;
-}
-else { 
-    interval.innerHTML = `${minutes} minutes`;
+// Modify message in the popup
+if (minutes < 1) {
+	minutes = minutes * 60;
+	timeRemaining.innerHTML = `${minutes} seconds`;
+} else if (minutes === 1) {
+	timeRemaining.innerHTML = `${minutes} minute`;
+} else {
+	timeRemaining.innerHTML = `${minutes} minutes`;
 }
 
 // End of pop-up //
-
-
-
 
 // Articles //
 
 // Add click event listeners to category buttons
 document.querySelectorAll('.category').forEach((button) => {
-    button.addEventListener('click', () => {
-        const category = button.id;
-        filterArticles(category);
-    });
+	button.addEventListener('click', () => {
+		const category = button.id;
+		filterArticles(category);
+	});
 });
 
-// Function to filter articles
+// Filter articles
 const filterArticles = (category) => {
-    const articles = document.querySelectorAll('.article');
-    articles.forEach((article) => {
-        if (article.getAttribute('data-category') === category || category === 'all') {
-            article.style.display = ''; // Show the article
-        } else {
-            article.style.display = 'none'; // Hide the article
-        }
-    });
-}
+	const articles = document.querySelectorAll('.article');
+	articles.forEach((article) => {
+		if (article.getAttribute('data-category') === category || category === 'all') {
+			article.style.display = ''; // Show the article
+		} else {
+			article.style.display = 'none'; // Hide the article
+		}
+	});
+};
 
 // End of articles //
-
