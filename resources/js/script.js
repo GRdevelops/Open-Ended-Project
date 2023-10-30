045bb035c2df6afe38aa124845f107bf5ce07e3a
GRdevelops <gianniruggieri99@gmail.com>
2023-10-30 16:55:29 +0100

    tablet responsivess and mobile navbar

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,49 +1,49 @@
 // Pop-up //
 
-const modal = document.getElementById('myModal');
-const closeButton = document.getElementsByClassName('close')[0];
+// const modal = document.getElementById('myModal');
+// const closeButton = document.getElementsByClassName('close')[0];
 
-const timeToAppear = 4000; // Milliseconds after which the pop-up will appear
-const timeInterval = 60 * 1000; // <-- Modify this. Prevent the pop-up from reappearing when reloading: Insert interval in milliseconds.
-let minutes = timeInterval / 1000 / 60; // Convert milliseconds to minutes
+// const timeToAppear = 4000; // Milliseconds after which the pop-up will appear
+// const timeInterval = 60 * 1000; // <-- Modify this. Prevent the pop-up from reappearing when reloading: Insert interval in milliseconds.
+// let minutes = timeInterval / 1000 / 60; // Convert milliseconds to minutes
 
-const modalClosedTime = localStorage.getItem('modalClosed');
-let currentTime = new Date().getTime();
+// const modalClosedTime = localStorage.getItem('modalClosed');
+// let currentTime = new Date().getTime();
 
-closeButton.addEventListener('click', () => {
-	modal.style.display = 'none';
-	currentTime = new Date().getTime(); // current timestamp in milliseconds
-	localStorage.setItem('modalClosed', currentTime.toString());
-});
-
-if (!modalClosedTime || currentTime - modalClosedTime >= timeInterval) {
-	setTimeout(() => {
-		modal.style.display = 'block';
-	}, timeToAppear);
-}
-
-// // Close the pop-up by clicking outside the window
-// window.addEventListener('click', (event) => {
-//     if (event.target == modal) {
-//         modal.style.display = 'none';
-//         currentTime = new Date().getTime(); // current timestamp in milliseconds
-//         localStorage.setItem('modalClosed', currentTime.toString());
-//     }
+// closeButton.addEventListener('click', () => {
+// 	modal.style.display = 'none';
+// 	currentTime = new Date().getTime(); // current timestamp in milliseconds
+// 	localStorage.setItem('modalClosed', currentTime.toString());
 // });
 
-// localStorage.removeItem('modalClosed'); // Uncomment to reset pop-up
+// if (!modalClosedTime || currentTime - modalClosedTime >= timeInterval) {
+// 	setTimeout(() => {
+// 		modal.style.display = 'block';
+// 	}, timeToAppear);
+// }
 
-const timeRemaining = document.getElementById('time-interval');
+// // // Close the pop-up by clicking outside the window
+// // window.addEventListener('click', (event) => {
+// //     if (event.target == modal) {
+// //         modal.style.display = 'none';
+// //         currentTime = new Date().getTime(); // current timestamp in milliseconds
+// //         localStorage.setItem('modalClosed', currentTime.toString());
+// //     }
+// // });
 
-// Modify message in the popup
-if (minutes < 1) {
-	minutes = minutes * 60;
-	timeRemaining.innerHTML = `${minutes} seconds`;
-} else if (minutes === 1) {
-	timeRemaining.innerHTML = `${minutes} minute`;
-} else {
-	timeRemaining.innerHTML = `${minutes} minutes`;
-}
+// // localStorage.removeItem('modalClosed'); // Uncomment to reset pop-up
+
+// const timeRemaining = document.getElementById('time-interval');
+
+// // Modify message in the popup
+// if (minutes < 1) {
+// 	minutes = minutes * 60;
+// 	timeRemaining.innerHTML = `${minutes} seconds`;
+// } else if (minutes === 1) {
+// 	timeRemaining.innerHTML = `${minutes} minute`;
+// } else {
+// 	timeRemaining.innerHTML = `${minutes} minutes`;
+// }
 
 // End of pop-up //
 
@@ -70,3 +70,36 @@ const filterArticles = (category) => {
 };
 
 // End of articles //
+
+// Mailchimp forms
+(function ($) {
+	window.fnames = new Array();
+	window.ftypes = new Array();
+	fnames[0] = 'EMAIL';
+	ftypes[0] = 'email';
+	fnames[1] = 'FNAME';
+	ftypes[1] = 'text';
+	fnames[2] = 'LNAME';
+	ftypes[2] = 'text';
+	fnames[3] = 'ADDRESS';
+	ftypes[3] = 'address';
+	fnames[4] = 'PHONE';
+	ftypes[4] = 'phone';
+	fnames[5] = 'BIRTHDAY';
+	ftypes[5] = 'birthday';
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
+
+// Nav-bar
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+const handleMenu = () => {
+	console.log("Hamburger clicked!");
+	hamburger.classList.toggle('active');
+	navMenu.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', handleMenu);
+
+// End of nav-bar
